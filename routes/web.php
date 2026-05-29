33ae6c8e807a79c726caff7c23338cc8949f537c
kitczy <krisniel.gonzales@cvsu.edu.ph>
2026-05-30 00:49:03 +0800

    fix auth route duplication

--- routes/web.php
+++ routes/web.php
@@ -8,65 +8,55 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\InquiryController;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 require __DIR__.'/auth.php';
 
-Auth::routes();
-
 Route::get('/home', function () {
     return redirect('/dashboard');
 });
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
+Route::view('/about', 'about')->name('about');
 
-Route::view('/about', 'about')
-    ->name('about');
-
-Route::view('/contact', 'contact')
-    ->name('contact');
+Route::view('/contact', 'contact')->name('contact');
 
 Route::post('/contact/send', [InquiryController::class, 'store'])
     ->name('contact.send');
 
 Route::middleware(['auth', 'prevent-back-history'])->group(function () {
 
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
 
-Route::get('/users', [DashboardController::class, 'users'])
-    ->name('users');
+    Route::get('/users', [DashboardController::class, 'users'])
+        ->name('users');
 
-Route::post('/users/store', [UserController::class, 'store'])
-    ->name('users.store');
+    Route::post('/users/store', [UserController::class, 'store'])
+        ->name('users.store');
 
-Route::put('/users/update/{id}', [UserController::class, 'update'])
-    ->name('users.update');
+    Route::put('/users/update/{id}', [UserController::class, 'update'])
+        ->name('users.update');
 
-Route::delete('/users/delete/{id}', [UserController::class, 'destroy'])
-    ->name('users.destroy');
+    Route::delete('/users/delete/{id}', [UserController::class, 'destroy'])
+        ->name('users.destroy');
 
-Route::get('/assignments', [AssignmentController::class, 'index'])
-    ->name('assignments');
+    Route::get('/assignments', [AssignmentController::class, 'index'])
+        ->name('assignments');
 
-Route::post('/assignments', [AssignmentController::class, 'store']);
+    Route::post('/assignments', [AssignmentController::class, 'store']);
 
-Route::put('/assignments/{id}', [AssignmentController::class, 'update']);
+    Route::put('/assignments/{id}', [AssignmentController::class, 'update']);
 
-Route::delete('/assignments/{id}', [AssignmentController::class, 'destroy']);
+    Route::delete('/assignments/{id}', [AssignmentController::class, 'destroy']);
 
-Route::get('/profile', [ProfileController::class, 'index'])
-    ->name('profile');
+    Route::get('/profile', [ProfileController::class, 'index'])
+        ->name('profile');
 
-Route::post('/profile/update', [ProfileController::class, 'update'])
-    ->name('profile.update');
+    Route::post('/profile/update', [ProfileController::class, 'update'])
+        ->name('profile.update');
 
-Route::post('/profile/photo', [ProfileController::class, 'uploadPhoto'])
-    ->name('profile.photo');
-
+    Route::post('/profile/photo', [ProfileController::class, 'uploadPhoto'])
+        ->name('profile.photo');
 });
